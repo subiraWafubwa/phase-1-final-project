c808187a20ba6989a69a3aa7c93118ed852b3531
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-07-23 12:46:15 +0300

    Update auth.js code

--- server/index.js
+++ server/index.js
@@ -1,26 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const userRouter = require('./routes/userRouter');
-const cors = require('cors');
+require('dotenv').config() 
+const express = require('express') 
+const path = require('path')
+const bodyParser = require('body-parser') 
+const morgan = require('morgan') 
+const userRouter = require('./routes/userRouter')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
 // Serve static files from the 'frontend' directory
 app.use(express.static(path.join(__dirname, 'frontend')));
 
-app.use(bodyParser.json());
-app.use(morgan('dev'));
-app.use(cors());
+app.use(bodyParser.json()) 
+app.use(morgan('dev'))
+app.use(cors()) 
 
 // Use the userRouter for /api routes
-app.use('/api', userRouter);
+app.use('/api', userRouter) 
 
-// Listen on a specific IP address and port
-const host = 'https://subnote-github-io-server.onrender.com'; // Change to your desired IP address if needed
-
-app.listen(host, () => {
-    console.log(`✅ Server is running on ${host}`);
-});
+app.listen(process.env.PORT,  () => {
+    console.log(`✅ Server is running`) 
+}) 
